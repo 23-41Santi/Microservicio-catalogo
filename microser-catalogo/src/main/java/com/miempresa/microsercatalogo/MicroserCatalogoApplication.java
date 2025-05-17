c035a01b6cf02fb16e1286ae8260c434aa9be694
Santiago Arenas <dilansanare@gmail.com>
2025-05-17 14:15:43 -0500

    primer commit

---
+++ microser-catalogo/src/main/java/com/miempresa/microsercatalogo/MicroserCatalogoApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa.microsercatalogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserCatalogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserCatalogoApplication.class, args);
+    }
+
+}
---
+++ microser-catalogo/src/test/java/com/miempresa/microsercatalogo/MicroserCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miempresa.microsercatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserCatalogoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
